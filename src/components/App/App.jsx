90da2f1f287b210ffcd2bd8b372257edce982495
Serhii Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-05-23 15:36:24 +0300

    create project structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,22 @@
-import './App.css'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { useId } from 'react';
+import * as Yup from 'yup';
+
+import './App.css';
+import ContactForm from '../ContactForm/ContactForm';
+import SearchBox from '../SearchBox/SearchBox';
+import ContactList from '../ContactList/ContactList';
 
 function App() {
-  
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+
 }
 
-export default App
+export default App;
